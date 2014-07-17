eb91853872e30d05ab0efe035fea07776301a298
Jesus Magana <maganazero5@gmail.com>
2014-07-16 21:33:17 -0700

    added spining colorful geometry

--- HelloGLKit/HelloGLKitViewController.m
+++ HelloGLKit/HelloGLKitViewController.m
@@ -8,17 +8,43 @@
 
 #import "HelloGLKitViewController.h"
 
+typedef struct {
+    float Position[3];
+    float Color[4];
+} Vertex;
+
+const Vertex Vertices[] = {
+    {{1, -1, 0}, {1, 0, 0, 1}},
+    {{1, 1, 0}, {0, 1, 0, 1}},
+    {{-1, 1, 0}, {0, 0, 1, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+const GLubyte Indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+
 @interface HelloGLKitViewController () {
     float _curRed;
     BOOL _increasing;
     
+    GLuint _vertexBuffer;
+    GLuint _indexBuffer;
+    
+    float _rotation;
+    
 }
+
+@property (strong, nonatomic) GLKBaseEffect *effect;
+
 @property (strong, nonatomic) EAGLContext *context;
 
 @end
 
 @implementation HelloGLKitViewController
 @synthesize context = _context;
+@synthesize effect = _effect;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,6 +54,36 @@
     }
     return self;
 }
+
+// OpenGl code
+- (void)setupGL {
+    
+    [EAGLContext setCurrentContext:self.context];
+    
+    self.effect = [[GLKBaseEffect alloc] init];
+    
+    glGenBuffers(1, &_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+    
+}
+
+- (void)tearDownGL {
+    
+    [EAGLContext setCurrentContext:self.context];
+    
+    glDeleteBuffers(1, &_vertexBuffer);
+    glDeleteBuffers(1, &_indexBuffer);
+    
+    self.effect = nil;
+    
+}
+
+// End OpenGl code
 
 - (void)viewDidLoad
 {
@@ -41,11 +97,15 @@
     
     GLKView *view = (GLKView *)self.view;
     view.context = self.context;
+    
+    [self setupGL]; //gl
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
+    
+    [self tearDownGL];  //gl
     
     if ([EAGLContext currentContext] == self.context) {
         [EAGLContext setCurrentContext:nil];
@@ -66,6 +126,18 @@
     glClearColor(_curRed, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     
+    [self.effect prepareToDraw];
+    
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *) offsetof(Vertex, Position));
+    glEnableVertexAttribArray(GLKVertexAttribColor);
+    glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *) offsetof(Vertex, Color));
+    
+    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
+    
 }
 
 #pragma mark - GLKViewControllerDelegate
@@ -84,6 +156,15 @@
         _curRed = 0.0;
         _increasing = YES;
     }
+    
+    float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 4.0f, 10.0f);
+    self.effect.transform.projectionMatrix = projectionMatrix;
+    
+    GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -6.0f);
+    _rotation += 90 * self.timeSinceLastUpdate;
+    modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, GLKMathDegreesToRadians(_rotation), 0, 0, 1);
+    self.effect.transform.modelviewMatrix = modelViewMatrix;
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
