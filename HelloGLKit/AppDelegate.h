68276d7629f520a6de11843d053978aa876e876b
Jesus Magana <maganazero5@gmail.com>
2014-07-16 20:24:39 -0700

    Initial Commit

---
+++ HelloGLKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloGLKit
+//
+//  Created by Jesus Magana on 7/16/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloGLKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloGLKit
+//
+//  Created by Jesus Magana on 7/16/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
